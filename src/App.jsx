9e70d456e6864cb31df4c70661b357f5d5cf3789
Julie Hildabrand <jhildabrand257@gmail.com>
2024-02-22 20:19:03 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+import WordCard from './components/WordCard'
+import Login from './components/Login'
+
+function App() {
+
+  return (
+    <>
+    <h1>Online Digital Journal</h1>
+    <WordCard/>
+    <Login/>
+    {/* <Contributors/> */}
+    </>
+  );
+}
+
+export default App
